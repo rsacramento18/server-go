40c14099605e60c9bf4b446ed7f88236e7e11c70
sacramento <ricardo.sacramento18@gmail.com>
2024-09-06 07:49:14 +0100

    feat: first server in golang example

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Building REST APIs")
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /comment", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "return all comments")
+	})
+
+	mux.HandleFunc("GET /comment/{id}", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "return all comments")
+	})
+
+	mux.HandleFunc("POST /comment", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "post a new comment")
+	})
+
+	if err := http.ListenAndServe("localhost:8080", mux); err != nil {
+		fmt.Println(err.Error())
+	}
+}
